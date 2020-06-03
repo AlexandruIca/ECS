ddf5e917a12b869e6c202f66a0109ce166b6a629
Alexandru Ica <alexandruica703@gmail.com>
2020-06-03 21:47:47 +0300

    Add another test for `ecs::registry`

--- tests/registry_test.cpp
+++ tests/registry_test.cpp
@@ -41,3 +41,35 @@ TEST_CASE("[ecs::registry] Basic - creating two entities and iterating over them
     r.for_each<position>([](position& p) -> void { MESSAGE("Inner position: (" << p.x << ", " << p.y << ')'); });
     r.for_each<velocity>([](velocity& v) -> void { MESSAGE("Inner velocity: " << v.v); });
 }
+
+TEST_CASE("[ecs::registry] Creating more entities conditionally")
+{
+    constexpr double default_x = 2.0;
+    constexpr double default_y = 3.0;
+    constexpr double default_v = 5.0;
+    constexpr int num_entities = 1'000;
+
+    double pos_sum{ 0 };
+    double vel_sum{ 0 };
+
+    ecs::registry r{};
+
+    for(int i = 0; i < num_entities; ++i) {
+        auto e = r.create_entity();
+
+        if(i % 2 == 0) {
+            r.emplace<position>(e, default_x, default_y);
+        }
+        r.emplace<velocity>(e, default_v);
+    }
+
+    r.for_each<position>([&pos_sum](auto& pos) -> void {
+        pos_sum += pos.x;
+        pos_sum += pos.y;
+    });
+
+    r.for_each<velocity>([&vel_sum](auto& vel) -> void { vel_sum += vel.v; });
+
+    REQUIRE(pos_sum == doctest::Approx{ 2'500 });
+    REQUIRE(vel_sum == doctest::Approx{ 5'000 });
+}
