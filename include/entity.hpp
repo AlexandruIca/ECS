0f75b57ec3f1e06796b8b7d6e4f947a9d6de521d
Alexandru Ica <alexandruica703@gmail.com>
2020-06-03 18:04:58 +0300

    Implement `entity` class.

---
+++ include/entity.hpp
@@ -0,0 +1,33 @@
+#ifndef ECS_ENTITY_HPP
+#define ECS_ENTITY_HPP
+#pragma once
+
+#include "id_type.hpp"
+
+namespace ecs {
+
+class entity
+{
+private:
+    id_type m_id{ 0 };
+
+public:
+    constexpr entity() noexcept = default;
+    entity(entity const&) noexcept = default;
+    entity(entity&&) noexcept = default;
+    ~entity() noexcept = default;
+
+    explicit entity(id_type id);
+
+    auto operator=(entity const&) noexcept -> entity& = default;
+    auto operator=(entity&&) noexcept -> entity& = default;
+
+    [[nodiscard]] constexpr auto id() const noexcept -> id_type
+    {
+        return m_id;
+    }
+};
+
+} // namespace ecs
+
+#endif // !ECS_ENTITY_HPP
---
+++ src/entity.cpp
@@ -0,0 +1,10 @@
+#include "entity.hpp"
+
+namespace ecs {
+
+entity::entity(id_type const id)
+    : m_id{ id }
+{
+}
+
+} // namespace ecs
